2f8f3b62e77454a9cbab3b4992f7b6cc2b47b835
Kenyansa <kenyansajustine@gmail.com>
2023-12-24 19:05:18 +0300

    remove unneeded code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import {lusitana} from '@/app/ui/fonts';
 import Image from 'next/image';
 
@@ -44,7 +43,6 @@ export default function Page() {
           alt='screenshots of the dashboard project for mobile view'
           />
         </div>
-          <div className={styles.shape}></div>  
       </div>
     </main>
   );
