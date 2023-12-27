8bac5afa23c811f751e5440a24227109784de85f
Kenyansa <kenyansajustine@gmail.com>
2023-12-27 21:34:02 +0300

    import important files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+
 export default function Page() {
     return <p>Dashboard Page</p>;
   }
