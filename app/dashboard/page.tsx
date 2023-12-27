974025da2ec4c655b6df7ab5ac78f17a79e1f8c5
Kenyansa <kenyansajustine@gmail.com>
2023-12-27 21:37:16 +0300

    add dashboard page content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,5 +5,25 @@ import { lusitana } from '@/app/ui/fonts';
 
 
 export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+    return (
+      <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+    )
+  } 
