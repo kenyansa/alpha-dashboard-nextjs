0c9db03ca01476708c148d3cdb7459994ab1466f
Kenyansa <kenyansajustine@gmail.com>
2023-12-31 21:46:11 +0300

    create handleSearch function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,10 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  function handleSearch(term: string){
+    console.log(term);
+    
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
