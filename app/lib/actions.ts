ae5345e1860f367ae5c6d32d64b1a995cdfdb206
Kenyansa <kenyansajustine@gmail.com>
2024-01-02 23:21:25 +0300

    create error.tsx for handling all errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,6 +60,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
+    //Unreachable code block
+    
+    try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
   }
