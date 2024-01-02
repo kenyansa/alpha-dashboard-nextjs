c4cc81b6dba0fc701b307b2d80dcf2dfec405b8a
Kenyansa <kenyansajustine@gmail.com>
2024-01-02 17:51:06 +0300

    Type validation and coercion using Zod TypeScript validation lib

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,17 @@
 'use server';
+import { z } from "zod";
 
-export async function createInvoice(formData: FormData) {}
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        
+    };
+    //checking:
+    console.log(rawFormData);
+}
