5e4154361ed0eb250e78a1f9b2f667259e8f05f1
Kenyansa <kenyansajustine@gmail.com>
2024-01-02 21:52:25 +0300

    server-action edit invoice

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
