501635ec7fd777ce5196e3923dae9362d28a4366
Kenyansa <kenyansajustine@gmail.com>
2024-01-02 21:58:08 +0300

    fetch both the invoice and customers in parallel with Promise.all

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
-export default async function Page() {
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+    ]);
   return (
     <main>
       <Breadcrumbs
