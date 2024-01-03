d1e2b3b36729fdf7efe028ae27c487d41275987a
Kenyansa <kenyansajustine@gmail.com>
2024-01-03 11:16:05 +0300

    increae acessibility by error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,13 +21,32 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
+export async function createInvoice(prevState: State, formData: FormData) {
+     // Validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
-   
+   // If form validation fails, return errors early. Otherwise, continue
+    if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Create Invoice.',
+        };
+      }
+    
+      // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
    
@@ -42,8 +61,10 @@ export async function createInvoice(formData: FormData) {
       };
     }
    
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+   
+    // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
   }
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -53,8 +74,6 @@ export async function updateInvoice(id: string, formData: FormData) {
       status: formData.get('status'),
     });
   
-    const amountInCents = amount * 100;
-  
     await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
@@ -62,7 +81,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
 
     revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    // redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
